661b4d502175075dce5a1bc5f3d12b14854a310e
Pablo Moyano <pamoyano@miuandes.cl>
2020-04-01 18:58:29 -0300

    First commit lab3

---
+++ Lab3/Lab3/Class2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class Producto
+    {
+        private string nombre;
+        private string marca;
+        private int precio;
+        private int stock;
+
+    }
+}
---
+++ Lab3/Lab3/Class3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class Worker:Persona
+    {
+        private string Job;
+        private string Wage;
+        private string timeshift;
+    }
+}
